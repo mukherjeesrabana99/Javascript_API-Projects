e2ebfce7cab8026f4be2736e9978dbc88179c024
Srabana Mukherjee <65303369+mukherjeesrabana99@users.noreply.github.com>
2021-01-27 13:22:23 +0530

    Add files via upload

--- movie_app/index.js
+++ movie_app/index.js
@@ -2,15 +2,13 @@ const form=document.querySelector("#form");
 const search=document.querySelector("#search");
 const main=document.getElementById("main");
 const btns=document.querySelectorAll(".btn")
-//API URLS
 const highest_rated_url="https://api.themoviedb.org/3/discover/movie?certification_country=US&certification=R&sort_by=vote_average.desc&primary_release_date.lte=2014-10-22&api_key=ad46f36c2b4f56ecbe2b758da6569806"
 const kids_url="https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&primary_release_date.lte=2014-10-22&api_key=ad46f36c2b4f56ecbe2b758da6569806"
 const comedies_url="https://api.themoviedb.org/3/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc&api_key=ad46f36c2b4f56ecbe2b758da6569806"
 const drama_url="https://api.themoviedb.org/3/discover/movie?with_genres=18&sort_by=vote_average.desc&vote_count.gte=10&api_key=ad46f36c2b4f56ecbe2b758da6569806"
-const popular_url="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ad46f36c2b4f56ecbe2b758da6569806"
 const IMGPATH = "http://image.tmdb.org/t/p/w92";
 const searchURL="https://api.themoviedb.org/3/search/movie?api_key=ad46f36c2b4f56ecbe2b758da6569806&query="
-//parent function for calling api as well as displaying items
+const popular_url="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ad46f36c2b4f56ecbe2b758da6569806"
 async function loadMovies(url){
 	const response= await fetch(url);
     const data= await response.json();
@@ -19,7 +17,7 @@ async function loadMovies(url){
 
 }
 loadMovies(popular_url)
-//card or box will be generated for each of the items of the array of the fetched api
+
 displayMovie=items=>{
 	const all_items=items.map(item=>{
 		return `
@@ -27,10 +25,10 @@ displayMovie=items=>{
 		<div class="card" style="width: 17rem; height:auto;">
 		  <img class="card-img-top" src="${IMGPATH+item.poster_path}">
 		  <div class="card-body">
-		    <h5 class="card-text">${item.original_title}.</h5>
-		    <p class="card-text">${item.overview}.</p>
-		    <h6 class="card-text"> ${item.release_date}.</h6>
-		    <h6 class="card-text"> ${item.vote_average}.</h6>
+		    <h5 class="card-text">${item.original_title}</h5>
+		    <p class="card-text">${item.overview}</p>
+		    <h6 class="card-text"><span style="color:#7378c5;">Release-Date:</span> ${item.release_date}</h6>
+		    <h6 class="card-text"><span style="color:#7378c5;">Rating:</span>${item.vote_average}</h6>
 		  </div>
 		</div>
 		</div>
@@ -82,3 +80,52 @@ btns.forEach(btn=>{
 		}
 	})
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function f(){
+// 	// const url= "https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699";
+// 	// const url="https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699"
+// 	const url="https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=ad46f36c2b4f56ecbe2b758da6569806"
+// 	// const url="http://hp-api.herokuapp.com/api/characters"
+// 	// const url="api.openweathermap.org/data/2.5/weather?q=London&appid=cc67b7d0ce2317f2498711dcfda3bba4"
+// 	const response= await fetch(url);
+//     const data= await response.json();
+//     console.log(data)
+    
+
+// }
+// f();
