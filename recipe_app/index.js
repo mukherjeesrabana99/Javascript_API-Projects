d9c773fe703c6232bedda7ded3949cce8876485c
Srabana Mukherjee <65303369+mukherjeesrabana99@users.noreply.github.com>
2021-01-28 23:55:46 +0530

    Add files via upload

--- recipe_app/index.js
+++ recipe_app/index.js
@@ -1,18 +1,27 @@
+//ACCESS ALL DOM DIVS TO BE POPULATED BY API DATA
 const meal_container=document.querySelector("#meal-container");
 const form=document.getElementById("form")
 const search=document.querySelector("#search");
-const category=document.querySelector("#cat");
-const area=document.querySelector("#area");
+////////////////////////////////////////////////////////////
 const btns=document.querySelectorAll(".btn")
+//SEARCH URL TO FETCH SEARCHED MEAL
 const search_url="https://www.themealdb.com/api/json/v1/1/search.php?s="
+//RANDOM URL TO FETCH RANDOM MEAL
 const random_url="https://www.themealdb.com/api/json/v1/1/random.php"
-const cats_url="https://www.themealdb.com/api/json/v1/1/list.php?c=list"
-const area_url="https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+//GET CATEGORY AND AREA LIST URLS
+const cats_list_url="https://www.themealdb.com/api/json/v1/1/list.php?c=list"
+const area_list_url="https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+//////////////////////////////////////////////////////////////////////////
+//URLS TO GET DATA BASED ON FILTER PARAMETERS
+const cat_url="https://www.themealdb.com/api/json/v1/1/filter.php?c="
+const area_url="https://www.themealdb.com/api/json/v1/1/filter.php?a="
+//////////////////////////////////////////////////////////////////////////////
+//GIVE A CALL TO THE API TO FETCH DATA AND DISPLAY THEM ON DOM LOAD
 const loadMeals=async(url)=>{
 	const response=await fetch(url)
-	const meals=await response.json()
-	console.log(meals)
-	displayMeal(meals.meals)
+	const data=await response.json()
+	console.log(data)
+	displayMeal(data.meals)
 }
 loadMeals(random_url)
 displayMeal=(meals)=>{
@@ -25,6 +34,7 @@ displayMeal=(meals)=>{
 	})
 	meal_container.innerHTML=all_meals.join("");
 }
+//GIVE A CALL TO THE API TO FETCH DATA AND DISPLAY THEM ON SEARCH
 form.addEventListener("submit",(e)=>{
 	e.preventDefault();
 	if(search.value){
@@ -33,8 +43,11 @@ form.addEventListener("submit",(e)=>{
 	
 	search.value=""
 })
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//GIVE A CALL TO THE API TO FETCH CATEGORY LIST
 async function loadCats(){
-	const response=await fetch(cats_url)
+	const response=await fetch(cats_list_url)
 	const data=await response.json()
 	console.log(data.meals)
 	displayCat(data.meals)
@@ -43,13 +56,15 @@ async function loadCats(){
 displayCat=(cats)=>{
 	all_cats=cats.map(cat=>{
 		return`
-		<li class="cat-link">${cat.strCategory}</li>
+		<li style="cursor:pointer;" onClick="getFilteredMeal(this.textContent,cat_url+this.textContent)">${cat.strCategory}</li>
 		`
 	})
-	category.innerHTML=all_cats.join("");
+	meal_container.innerHTML=all_cats.join("");
 }
+///////////////////////////////////////////////////////////////
+//GIVE A CALL TO THE API TO FETCH AREA LIST
 async function loadArea(){
-	const response=await fetch(area_url)
+	const response=await fetch(area_list_url)
 	const data=await response.json()
 	
 	console.log(data.meals)
@@ -59,11 +74,13 @@ async function loadArea(){
 displayArea=(areas)=>{
 	all_area=areas.map(area=>{
 		return`
-		<li class="area-link">${area.strArea}</li>
+		<li style="cursor:pointer;" onClick="getFilteredMeal(this.textContent,area_url+this.textContent)">${area.strArea}</li>
 		`
 	})
-	area.innerHTML=all_area.join("");
+	meal_container.innerHTML=all_area.join("");
 }
+////////////////////////////////////////////////////////////////////
+//DISPLAY AREA AND CATEGORY LIST ON BTN CLICK
 btns.forEach(btn=>{
 	btn.addEventListener("click", function(){
 		switch(this.id){
@@ -78,3 +95,24 @@ btns.forEach(btn=>{
 		}
 	})
 })
+//////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+//GIVE A CALL TO THE API TO FETCH  DATA FILTERED BY CAT OR AREA AND DISPLAY THEM 
+//ON CLICKING ON ANY OF AREA OR CATEGORY NAMES
+async function getFilteredMeal(item,url){
+	console.log(item)
+	const response=await fetch(url)
+	const data=await response.json()
+	console.log(data)
+	displayFilteredMeal(data.meals)
+}
+displayFilteredMeal=(meals)=>{
+	filtered_meals=meals.map(meal=>{
+		return`
+		<img src="${meal.strMealThumb}">
+		<h3>${meal.strMeal}</h3>
+		`
+	})
+	meal_container.innerHTML=filtered_meals.join("");
+}
+///////////////////////////////////////////////////////////////////////////////
