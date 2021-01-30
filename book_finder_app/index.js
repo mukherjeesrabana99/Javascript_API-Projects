705d250d6a823a0c4cdb988181f8d6970fd574e6
Srabana Mukherjee <65303369+mukherjeesrabana99@users.noreply.github.com>
2021-01-30 13:37:35 +0530

    Add files via upload

--- book_finder_app/index.js
+++ book_finder_app/index.js
@@ -1,7 +1,6 @@
 const search_input=document.querySelector("#search-input");
 const search_btn=document.querySelector("#search-btn");
 const book_container=document.querySelector("#book-container");
-const heading=document.querySelector(".heading");
 const url= "https://www.googleapis.com/books/v1/volumes?q=";
 search_btn.addEventListener("click", function(){
 	loadBooks(url+search_input.value)
@@ -12,7 +11,6 @@ async function loadBooks(url){
 	const response= await fetch(url);
     const data= await response.json();
     console.log(data.items)
-    heading.innerHTML=`<h3 style="color:#7378c5;">Get similar books by clicking on the image</h3>`
     displayBooks(data.items)
 
 }
@@ -20,9 +18,10 @@ async function loadBooks(url){
 displayBooks=(books)=>{
 	all_books=books.map(book=>{
 		return `
+		
 		<div class="col-lg-3 col-md-6 col-sm-6">
 		<div class="card" style="width: 17rem; height:auto;">
-		  <img class="card-img-top" style="cursor:pointer;" onClick="getSimilar(this.id)" id="${book.id}" src="${book.volumeInfo.imageLinks.smallThumbnail}">
+		  <img class="card-img-top" style="cursor:pointer;"  src="${book.volumeInfo.imageLinks.smallThumbnail}">
 		  <div class="card-body">
 		    <b class="card-text" style="color:#7378c5;">${book.volumeInfo.title}</b>
 		    <p class="card-text"><span style="color:#7378c5;"><b>Written By: </b></span>${book.volumeInfo.authors}</p>
@@ -36,8 +35,4 @@ displayBooks=(books)=>{
 		`
 	})
 	book_container.innerHTML=all_books.join("")
-}
-getSimilar=(id)=>{
-	console.log(id);
-	loadBooks(url+id)
 }
